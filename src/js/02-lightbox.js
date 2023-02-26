d8c9e49d40ce03dd948606abfe2f8e1e0df1d625
Andrii Pysmennyi <andrey.ad511989@gmail.com>
2023-02-26 18:56:43 +0200

    finish 7 homework tast 2

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,17 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+// console.log(galleryItems);
 
-console.log(galleryItems);
+
+const gallereyMarkup = galleryItems.map(({original, preview, description}) => `
+<a class="gallery__item" href="${original}" title="${description}" >
+  <img class="gallery__image" src="${preview}" alt="${description}" captionPosition />
+</a>
+`).join('');
+
+const galleryEl = document.querySelector('.gallery');
+//рендер разметки
+galleryEl.insertAdjacentHTML('beforeend', gallereyMarkup);
+
+var lightbox = new SimpleLightbox('.gallery a', { captionsData:"alt",captionDelay: 1000,captionPosition: "bottom" });
+
